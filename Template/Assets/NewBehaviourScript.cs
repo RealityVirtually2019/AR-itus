4cd21dbe4773515500cf10576ab23d13ed961286
Mark Zhehai Zhang <zhehaizhang1@gmail.com>
2019-01-17 23:08:34 -0500

    Test Unity Upload

---
+++ Template/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour //Any class that is a monobehaviour can be attacged to gameobjects, public fields can be edited
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            print("Hello");
+        }
+    }
+}
---
+++ Template/Assets/Scripts/ClickPoker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPoker : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked");
+            //Raycasting for detecting if mouse is on the cube
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit!");
+
+                var collider = hit.collider;
+                Debug.Log(collider.name);
+            }
+        }
+    }
+}
