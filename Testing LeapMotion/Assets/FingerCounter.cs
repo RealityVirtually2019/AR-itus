9f497361cc66069e063b9c997ec2af4ac35f8292
Noor-Nasri-2 <46821000+Noor-Nasri-2@users.noreply.github.com>
2019-01-18 22:22:49 -0500

    Hand Gestures with Leap Motion for Numbers

---
+++ Testing LeapMotion/Assets/FingerCounter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerCounter : MonoBehaviour
+{
+    public Transform thumb, index, middle, ring, pinky;
+    private List<Transform> fingers = new List<Transform>();
+    // Start is called before the first frame update
+    private int previousNum = 0;
+    void Start()
+    {
+        Debug.Log("Starting");
+        fingers.Add(thumb);
+        fingers.Add(index);
+        fingers.Add(pinky);
+        fingers.Add(ring);
+        fingers.Add(middle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       int numFings = 0;
+       foreach (Transform curFing in fingers){
+            Transform nextFing = curFing;
+            while (true)
+            {
+                if (nextFing.childCount == 0)
+                {
+                    numFings++;
+                    break;
+                }
+                else if (nextFing.position.y>=nextFing.GetChild(0).position.y)
+                {
+                    break;           
+                }
+                else
+                {
+                    nextFing = nextFing.GetChild(0);
+                }
+            }
+        }
+        if (numFings != previousNum)
+        {
+            Debug.Log(numFings);
+            previousNum = numFings;
+        }
+    }
+}
