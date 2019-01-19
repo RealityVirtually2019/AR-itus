926d9d60974fe02fb276438bf12724f1c8d0e102
Noor-Nasri-2 <46821000+Noor-Nasri-2@users.noreply.github.com>
2019-01-19 10:31:19 -0500

    Added Accuracy Function For Hand Movement

--- Testing LeapMotion/Assets/FingerCounter.cs
+++ Testing LeapMotion/Assets/FingerCounter.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class FingerCounter : MonoBehaviour
 {
-    public Transform thumb, index, middle, ring, pinky;
+    public Transform thumb, index, middle, ring, pinky, joint_ti, joint_im, joint_mr, joint_rp;
     private List<Transform> fingers = new List<Transform>();
-    // Start is called before the first frame update
+    private List<Transform> accuracyJoints = new List<Transform>();
+    //Thumb-Index, Index-Middle, Middle-Ring, Ring-Pinky
+    private const float accuracyConst = 30f;
     private int previousNum = 0;
     void Start()
     {
         Debug.Log("Starting");
         fingers.Add(thumb);
         fingers.Add(index);
+        fingers.Add(middle);
         fingers.Add(pinky);
         fingers.Add(ring);
-        fingers.Add(middle);
+
+        accuracyJoints.Add(joint_ti);
+        accuracyJoints.Add(joint_im);
+        accuracyJoints.Add(joint_mr);
+        accuracyJoints.Add(joint_rp);
+
     }
 
     // Update is called once per frame
@@ -31,9 +39,13 @@ public class FingerCounter : MonoBehaviour
                     numFings++;
                     break;
                 }
-                else if (nextFing.position.y>=nextFing.GetChild(0).position.y)
+                else if (nextFing.position.y >= nextFing.GetChild(0).position.y)
                 {
-                    break;           
+                    break;
+                }
+                else if (Mathf.Rad2Deg*(Mathf.Atan(Mathf.Abs((nextFing.GetChild(0).position.y - nextFing.position.y) / (nextFing.GetChild(0).position.x - nextFing.position.x))))<50) 
+                {
+                    break;
                 }
                 else
                 {
@@ -46,5 +58,37 @@ public class FingerCounter : MonoBehaviour
             Debug.Log(numFings);
             previousNum = numFings;
         }
+        Debug.Log(Accuracy());
     }
+    float Accuracy()
+    {
+        float curAccuracy = 100.0f;
+        float DeviationFromStandard = 0;
+        for (int i = 1; i<fingers.Count; i++)
+        {
+            Transform endPoint_1 = FindEndPoint(fingers[i]);
+            Transform endPoint_2 = FindEndPoint(fingers[i - 1]);
+            float Side_InBetweenEndPoints = Vector3.Distance(endPoint_1.position, endPoint_2.position);
+            float Side_F1toJoint = Vector3.Distance(endPoint_1.position, accuracyJoints[i - 1].position);
+            float Side_F2toJoint = Vector3.Distance(endPoint_2.position, accuracyJoints[i - 1].position);
+            float theta = Mathf.Rad2Deg*Mathf.Acos((Mathf.Pow(Side_InBetweenEndPoints,2)-Mathf.Pow(Side_F1toJoint,2)-Mathf.Pow(Side_F2toJoint,2))/(-2*Side_F2toJoint*Side_F1toJoint));
+            DeviationFromStandard += Mathf.Abs(theta - accuracyConst)/accuracyConst;
+            Debug.Log(DeviationFromStandard);
+            
+        }
+        return 100 - DeviationFromStandard;
+    }
+    float dist(Vector3 p1, Vector3 p2)
+    {
+        return Vector3.Distance(p1, p2);
+    }
+    Transform FindEndPoint(Transform startFinger)
+    {
+        while (startFinger.childCount != 0)
+        {
+            startFinger = startFinger.GetChild(0);
+        }
+        return startFinger;
+    }
+    
 }
