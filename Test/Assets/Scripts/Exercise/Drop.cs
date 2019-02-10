a23bfe44fc7e4507735e5d324cdb02517f176205
Anish Aggarwal <anish.5@hotmail.com>
2019-02-09 19:42:35 -0500

    Comments on Drop.cs - Completed

--- Test/Assets/Scripts/Exercise/Drop.cs
+++ Test/Assets/Scripts/Exercise/Drop.cs
@@ -5,102 +5,107 @@ using UnityEngine;
 
 public class Drop : MonoBehaviour
 {
-    public GameObject responsibleUI;
-    public Transform rindex_end, rthumb_end, rmiddle_end, rpinky_end, rring_end, rpalm, palm;
-    public Transform lindex_end, lthumb_end, lmiddle_end, lpinky_end, lring_end, lpalm, rhand, lhand;
-    private List<Transform> fingers = new List<Transform>(), lfingers = new List<Transform>(), rfingers = new List<Transform>();
-    float HandPos1 = 0, HandPos2 = 0;
+    private GameObject responsibleUI;//Changes UI based on movement of fingers, time, etc.
 
-    // Use this for initialization
-    float startTime;
+    //Variables to track which hand is moving
+    private Transform rpalm, palm;
+    private Transform lpalm, rhand, lhand;
+
+    private List<Transform> fingers = new List<Transform>(), lfingers = new List<Transform>(), rfingers = new List<Transform>(); // Lists that hold the fingers on each hand. Fingers gets set to lfingers or rfingers depending on which hand is active. The finger parts are then checked to see if user has dropped the object.
+
+    float startTime;//get start time to get total time for display
+
+    int frame_counter = 0;//starts with frame_counter at -
+    int score = 0;//current score is 0
+
+    float high_score = 150;// highest stretch score - largest stretch hand
+    float low_score = 150;// lowest stretch score - smallest fist
+
     void Start()
     {
-        startTime = Time.realtimeSinceStartup;
-        rfingers.Add(rindex_end);
-        rfingers.Add(rthumb_end);
-        rfingers.Add(rmiddle_end);
-        rfingers.Add(rring_end);
-        rfingers.Add(rpinky_end);
+        //Initialization of Variables -------------------------------
+        responsibleUI = GameObject.Find("Canvas");//gets the UI interface
+        //Left Hand & Palm
+        lhand = GameObject.Find("Left Hand").transform;
+        lpalm = lhand.Find("Left Palm");
+        //Right Hand & Palm
+        rhand = GameObject.Find("Right Hand").transform;
+        rpalm = rhand.Find("Right Palm");
 
-        lfingers.Add(lindex_end);
-        lfingers.Add(lthumb_end);
-        lfingers.Add(lmiddle_end);
-        lfingers.Add(lring_end);
-        lfingers.Add(lpinky_end);
+        palm = rpalm;//Right hand starts as active. Then based on movement we set palm to be whatever is active. 
+
+        startTime = Time.realtimeSinceStartup;//get time for displaying time during course
+
+        foreach (Transform right_finger in rhand)//For each finger in the right hand we add that to the right fingers
+        {
+            rfingers.Add(right_finger);
+        }
+        foreach(Transform l_finger in lhand)//For each finger in the left hand we add that to the left fingers
+        {
+            lfingers.Add(l_finger);
+        }
+        fingers = rfingers;//set fingers to rhand at start
     }
-
-    // Update is called once per frame
-
-
-    // Update is called once per frame
-    int i = 0;
-    float totAccuracy = 0;
-    int score = 0;
-
-    float high_score = 150;
-    float low_score = 150;
 
     void Update()
     {
-        i++;
-        if (i >= 1000)
+        frame_counter++;//add one frame counter every frame
+        //when frame counter is greater then 1000, a decent number of tries has  happened, so reset high score and low score so user can try to stretch an compress hand again.
+        if (frame_counter >= 1000)
         {
-            i = 0;
+            frame_counter = 0;//set frame_counter to 0 again
+            
+            //set high score and low score to 0 so that score will not increase until maximum difference is created
             high_score = 150;
             low_score = 150;
         }
 
-        if (rhand.gameObject.activeSelf)
+        if (rhand.gameObject.activeSelf)//if right hand is active set palm and fingers to right
         {
             palm = rpalm;
             fingers = rfingers;
         }
-        else if (lhand.gameObject.activeSelf)
+        else if (lhand.gameObject.activeSelf)//else set palm and fingers to left
         {
             palm = lpalm;
             fingers = lfingers;
         }
+        float cur_score = 100 * (GetHandPos());//current score calculates by the position of the hand
 
-        //last_score = cur_score;
-        float cur_score = 100 * (GetHandPos()) / 0.31f;
-        if (cur_score < low_score)
+        if (cur_score < low_score)//if the current score is less than the low score, set low score to be greater than cur_score
         {
             low_score = cur_score;
-            score += Mathf.Abs((int)((high_score - low_score) / 10));
-        }else if (cur_score > high_score)
+            score += Mathf.Abs((int)((high_score - low_score) / 10));//add to score the difference between the scores divided by a stabilization constant
+        }
+        else if (cur_score > high_score)//if the current score is greater than the high score we set the high score
         {
             high_score = cur_score;
-            score += Mathf.Abs((int)((high_score - low_score) / 10));
+            score += Mathf.Abs((int)((high_score - low_score) / 10));//add to score the difference between the scores divided by a stabilization constant
         }
-        responsibleUI.transform.Find("Timer").GetComponent<TextMesh>().text = "Time: " + ((int)(Time.realtimeSinceStartup - startTime)).ToString();
-        responsibleUI.transform.Find("Score").GetComponent<TextMesh>().text = "Score: " + score.ToString();
-
-        //Debug.Log(Vector3.Distance(palm.position, pinky_end.position));
-
-        // Debug.Log(GetHandPos());
-        
+        responsibleUI.transform.Find("Timer").GetComponent<TextMesh>().text = "Time: " + ((int)(Time.realtimeSinceStartup - startTime)).ToString();//Update Time of what player sees
+        responsibleUI.transform.Find("Score").GetComponent<TextMesh>().text = "Score: " + score.ToString();//update score of player
     }
-    float GetHandPos()
+
+    float GetHandPos()//Gets the Hand Position Value by taking the sums of all finger position from the palm position 
     {
-        float sumVecs = 0;
+        float sumVecs = 0;//starts at 0
         foreach (Transform fing in fingers)
         {
-            sumVecs += Vector3.Distance(fing.position, palm.position);
+            sumVecs += Vector3.Distance(fing.position, palm.position);//adds distance to each finger, thus seeing if it is completely stretched out or a fist
         }
-        return sumVecs;
+        return sumVecs;//return current position value
 
     }
 
     private void OnDestroy()
     {
         int lastScore = int.Parse(responsibleUI.transform.parent.Find("Exercise Selection").Find("stretch - score").gameObject.GetComponent<TextMesh>().text.Substring(6));
-        Debug.Log(lastScore);
-        Debug.Log(score);
         if (lastScore < score + 100)
         {
             responsibleUI.transform.parent.Find("Exercise Selection").Find("stretch - score").gameObject.GetComponent<TextMesh>().text = "Goal: " + (score + 100).ToString();
         }
 
+        //When user exits the game write to the text file that stores user scores along with the current date and time
         StreamWriter writer = new StreamWriter("Assets/Stretch_Test.txt", true);
         writer.WriteLine(System.DateTime.Now + " - Score: " + score);
         writer.Close();
